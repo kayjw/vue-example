d9e50e44dc8c153470adc63cf1e205c1a3abb9e1
Kay Wild <kwild01@ch-c02z42m7lvdq.home>
2022-01-29 16:54:24 +0100

    add examples

---
+++ index.js
@@ -0,0 +1,64 @@
+var app = new Vue({
+    el: "#app",
+    data: {
+        name: "Max Mustermann"
+    },
+    methods: {
+        changeName: function (event) {
+            this.name = event.target.value;
+        }
+    }
+});
+
+var app2 = new Vue({
+    el: '#app-2',
+    data: {
+        message: 'You loaded this page on ' + new Date().toLocaleString()
+    }
+})
+
+var app3 = new Vue({
+    el: '#app-3',
+    data: {
+        seen: true,
+        btnText: "Hide"
+    },
+    methods: {
+        removeText: function () {
+            this.seen = false;
+            this.btnText = "Wow"
+        }
+    }
+})
+
+var app4 = new Vue({
+    el: '#app-4',
+    data: {
+        todos: [
+            { text: 'Learn JavaScript' },
+            { text: 'Learn Vue' },
+            { text: 'Build something awesome' }
+        ]
+    }
+})
+app4.todos.push({ text: 'New item' })
+
+
+var app5 = new Vue({
+    el: '#app-5',
+    data: {
+        message: 'Hello Vue.js!'
+    },
+    methods: {
+        reverseMessage: function () {
+            this.message = this.message.split('').reverse().join('')
+        }
+    }
+})
+
+var app6 = new Vue({
+    el: '#app-6',
+    data: {
+        message: 'Hello Vue!'
+    }
+})
